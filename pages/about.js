eb769ba3d8a6901d90c31b23941369bcd5700008
Carl Ogren <cogren.external@lafourchette.com>
2018-10-01 15:02:45 +0200

    More description

--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,10 @@ const AboutPage = () => (
       <Content>
         <p>
           This project was developed by myself, <a href='https://carlogren.com'>Carl Ogren</a>,
-          as an example to show off <a href='https://nextjs.org/'>Next.js</a> during
-          the <a href='https://www.meetup.com/fr-FR/React-Nantes/'>React Nantes meetup</a>.
+          as an example to showcase <a href='https://nextjs.org/'>Next.js</a> during a live coding presentation at
+          the <a href='https://www.meetup.com/fr-FR/React-Nantes/'>React Nantes meetup</a>. The actual
+          site that was developed during the presentation was a bit simpler than this one to keep
+          the audience focused on the real subject (the code not the beers).
         </p>
         <p>The data was fetched from the awesome <a href='https://punkapi.com/'>PunkAPI</a>.</p>
         <p>The source code is open source and can be found on <a href='https://github.com/RasCarlito/next-brew'>Github</a>.</p>
--- readme.md
+++ readme.md
@@ -1,13 +1,15 @@
 # next-brew
 
 A demo application using Next.js to display Brewdog beers using Punk API.
-This was developed as an example to showcase Next.js at the [React Nantes](https://www.meetup.com/fr-FR/React-Nantes) meetup.
+
+You can see the [result here](https://next-brew.carlogren.com).
+
+This was developed as an example to showcase Next.js at the [React Nantes](https://www.meetup.com/fr-FR/React-Nantes) meetup. The presentation was a live coding developing a simpler version of this site.
 
 Thanks to:
 - [Next.js](https://nextjs.org) for an awesome framework wrapping React
 - [punkapi.com](https://punkapi.com) for listing all of the awesome Brewdog beer recipes
 - [bulma.io](https://bulma.io) for an awesome css framework
-- And always some many more open-source things to use!
 
 ## Installation
 
